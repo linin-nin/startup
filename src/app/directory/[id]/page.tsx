a2004c636291d082544ddfcd0bac60a4debf05b7
Ninn04 <lynin722@gmail.com>
2023-12-27 08:16:27 +0700

    update

--- src/app/directory/[id]/page.tsx
+++ src/app/directory/[id]/page.tsx
@@ -1,45 +1,67 @@
-/* eslint-disable react-hooks/rules-of-hooks */
-import React, { useEffect, useState } from 'react';
-import { createDirectus, rest, readItems } from '@directus/sdk';
-import { useRouter } from 'next/router';
+import React from 'react'
+import { createDirectus, rest, readItems } from '@directus/sdk'
+
+interface Startup {
+  id: number;
+  company_name: string;
+  // Add other properties based on your data structure
+}
+interface StartupDetailPageProps {
+  startup: Startup[];
+}
 
 const client = createDirectus("https://startupnation.panel.dreamslab.dev/").with(rest());
 
-async function fetchdata(slug:string){
-  return client.request(
-    readItems("Startups",{
-      fields: ['*'],
-      filter: {slug},
-      single: true
-    })
-  )
-}
+function fetchData(slug:string) {
+    return client.request(
+      readItems("Startups", {
+        fields: [
+          'id',
+          'company_name', 
+          'title', 
+          { founder: ['*']},
+          {category: ['category_name']}
+        ],
+        filter: {
+          slug: slug
+        }
+      })
+    );
+  }
 
-const page = () => {
-  // eslint-disable-next-line react-hooks/rules-of-hooks
-  const router = useRouter();
-  const { slug } = router.query;
-  const [startupData, setStartupData] = useState(null);
-  const [loading, setLoading] = useState(true);
+  export async function getServerSideProps(params) {
+    try {
+      const slug = params.slug;
+      const data = await fetchData(slug);
+  
+      return {
+        props: {
+          startup: data,
+        },
+      };
+    } catch (error) {
+      console.error('Error fetching data:', error);
+      return {
+        props: {
+          startup: null,
+        },
+      };
+    }
+  }
 
-  useEffect(() => {
-    const getStartupdata = async () => {
-      try {
-        const result = await fetchdata(slug);
-        setStartupData(result);
-      } catch (error) {
-        console.error('Error fetching startup data:', error);
-      } finally {
-        setLoading(false);
-      }
-    }
-    if (slug) {
-      getStartupdata();
-    }
-  }, [slug])
+
+const Detailpage: React.FC<StartupDetailPageProps> = ({startup}) => {
   return (
-    <div>page</div>
+    <div>
+      {
+        startup.map((item) => {
+          <div key={item.id}>
+
+          </div>
+        })
+      }
+    </div>
   )
 }
 
-export default page
+export default Detailpage
